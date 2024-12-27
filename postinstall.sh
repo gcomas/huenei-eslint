1e218ee7f44d991264df1bc713c0cf529d9c26a4
Gabriel Comas <gcomas@huenei.com>
2024-12-27 16:29:10 -0300

    choe: remove unused file

--- postinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Post install script is running..."
-npm install @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-semistandard eslint-plugin-n eslint-plugin-oxlint eslint-plugin-promise globals standard typescript
