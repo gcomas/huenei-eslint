d80da1cb8eaa660df198d86f47de8c07d1f9ef55
Gabriel Comas <gcomas@huenei.com>
2024-12-30 11:55:03 -0300

    feat: add eslint validation before build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup'
+import { execSync } from 'child_process'
 
 export default defineConfig({
     format: ['cjs', 'esm'],
@@ -6,5 +7,16 @@ export default defineConfig({
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
-    clean: true
+    clean: true,
+    onSuccess: async () => {
+        try {
+            console.log('Running ESLint...')
+            execSync('npx eslint src', { stdio: 'inherit' })
+        } catch (error) {
+            console.error('ESLint failed. Fix the errors before building.')
+            console.error(error)
+            process.exit(1)
+        }
+        return Promise.resolve()
+    },
 })
